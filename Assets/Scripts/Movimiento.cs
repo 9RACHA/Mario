f30f090a96459ea1f5872a0acf026b441aebd28e
Racha <9rachadev@gmail.com>
2021-12-02 21:54:11 +0100

    Animaciones Mario

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
     private float speed = 5f;
+    Animator animator; //Declarar animator
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GetComponent muy polivalente
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,9 +19,15 @@ public class Movimiento : MonoBehaviour
     {
         //Transform para usarlo hay que acceder a la variabble que hereda de la clase mono beheavour
         if(Input.GetKey(KeyCode.LeftArrow)) { //Si pulso la tecla izquierda
-            transform.Translate(Vector3.left * speed * Time.deltaTime);     //Transform para usarlo hay que acceder a la variable que hereda de la clase MonoBehaviour
+            transform.Translate(Vector3.left * speed * Time.deltaTime);     //Mediante translate lleva al punto de movimiento 
+            animator.SetBool("Camina", true); //Al ser V o F 
+            transform.localScale = new Vector3(1, 1, 1); // Mira a la izquierda
         } else if(Input.GetKey(KeyCode.RightArrow)) {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+            animator.SetBool("Camina", true);
+            transform.localScale = new Vector3(-1, 1, 1); // Mira a la derecha xq la animacion de Mario estaba hacia ese lado
+        } else {
+            animator.SetBool("Camina", false);
+        }
         }
     }
-}
